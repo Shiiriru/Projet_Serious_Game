e05dc9a9328dbc9224d1024c519b936f9ee03101
Shiiriru <c.alvergnat.pro@gmail.com>
2020-05-11 18:06:25 +0200

    Cyrille = Mise en place de la première version de l'OnBoarding

---
+++ Assets/Script/AppelCommandement.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AppelCommandement : MonoBehaviour
+{
+    public Text TexteOnboarding;
+
+    public GameObject SupportTexteOnboarding;
+    public GameObject Objets_Onboard;
+    public GameObject Objets_Active;
+    public GameObject MusiqueFond;
+
+    float NbMessage = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SupportTexteOnboarding.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(OnBoarding.FirstSpeechDone == true)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                SwitchTexteCmd();
+                NbMessage += 1;
+            }
+        }
+    }
+
+    public void SwitchTexteCmd()
+    {
+        SupportTexteOnboarding.SetActive(true);
+
+        if (NbMessage == 0)
+        {
+            TexteOnboarding.text = "Capitaine Favourier, bienvenue au front !Vous arrivez juste à temps avant la bataille !";
+        }
+
+        if (NbMessage == 1)
+        {
+            TexteOnboarding.text = "Cependant, je dois vous avouez que je ne suis pas forcément confiant envers vos capacités, vous débarquez direct de l'école et vous n'y connaissez rien au combat ...";
+        }
+
+        if (NbMessage == 2)
+        {
+            TexteOnboarding.text = "Nos troupes ici sont cruciales, et la bataille importante. Cependant, nous n'avons aucune visibilité sur votre situation. Il va falloir que vous nous fassiez le bilan vous même, ça va nous permettre de voir si vous êtes au point !";
+        }
+
+        if (NbMessage == 3)
+        {
+            TexteOnboarding.text = "Tout d'abord, veillez consulter la carte de bataille et prendre contact l'artillerie, ce serait dommage de ne pas profiter d'eux n'est ce pas ? Aussi, si vous sortez, n'oubliez pas de prendre la lettre de renseignement pour nos amis anglais.";
+        }
+
+        if (NbMessage == 4)
+        {
+            TexteOnboarding.text = "Quand vous serez prêts, recontactez moi ! Fin de transmission.";
+        }
+
+        if (NbMessage == 5)
+        {
+            SupportTexteOnboarding.SetActive(false);
+
+            Objets_Onboard.SetActive(false);
+            Objets_Active.SetActive(true);
+
+            MusiqueFond.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Script/OnBoarding.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnBoarding : MonoBehaviour
+{
+    public Text TexteOnboarding;
+
+    public GameObject SupportTexteOnboarding;
+
+    float NbMessage = 0;
+
+    public static bool FirstSpeechDone = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TexteOnboarding.text = "La première guerre mondiale ... Depuis 1914, le conflit dure, s'étendant partout. Notr epays a été attaqué par les allemands suite à une escalade de tension.";
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SwitchTexte();
+            NbMessage += 1;
+        }
+    }
+
+    void SwitchTexte()
+    {
+        if (NbMessage == 0)
+        {
+            TexteOnboarding.text = "Depuis 2 ans, nos braves soldats se battent partout, bloquant la progression ennemi ... La Marne, Verdun ... De grandes batailles qui ont montré notre détermination à bouter l'envahisseur hors de notre territoire !";
+        }
+
+        if (NbMessage == 1)
+        {
+            TexteOnboarding.text = "Depuis 1915, nous sommes bloqués dans les tranchées et la boue, livrant une sale guerre de position. Cependant, nous sommes en 1916 et nos chefs ont enfin décidé d'attaquer conjointement avec les anglais. C'est la première fois de la guerre que nous le faisons.";
+        }
+
+        if (NbMessage == 2)
+        {
+            TexteOnboarding.text = "Il parrait que les britanniques ont une nouvelle armes contre les germains, que ça va enfin briser la guerre de mouvement et permettre de reprendre l'initiative, je me demande de quoi il s'agit ...";
+        }
+
+        if (NbMessage == 3)
+        {
+            TexteOnboarding.text = "Je me présente, Célestin Favourier, jeune capitaine du 212eme Régiment d'Infanterie de l'armée française. Je viens tout juste d'arriver au front ... Et ceci est mon histoire.";
+        }
+
+        if (NbMessage == 4)
+        {
+            SupportTexteOnboarding.SetActive(false);
+            FirstSpeechDone = true;
+        }
+    }
+}
