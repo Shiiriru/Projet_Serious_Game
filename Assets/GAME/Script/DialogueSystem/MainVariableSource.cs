058c1a428fab6c1edaaf6abc67b20b81555f9160
Shiiriru <c.alvergnat.pro@gmail.com>
2020-06-05 17:01:16 +0200

    Cyrille = Integration conditions et variables dans dialog system

--- Assets/GAME/Script/DialogueSystem/MainVariableSource.cs
+++ Assets/GAME/Script/DialogueSystem/MainVariableSource.cs
@@ -5,11 +5,26 @@ using DialogSystem;
 public class MainVariableSource : VariableSource, IMainVariableSource
 {
 	public bool LetterChecked;
+    public bool MapChecked;
+    public bool PhotoChecked;
+    public bool GameSpotGermanFinished;
+    public bool MaskChecked;
+    public bool CardGameWon;
 
 	public void OnBordingSelectPhotoFace(int index){ }
+    public void LaunchGameSpotGerman() { }
+    public void LaunchCardGame() { }
+    public void LaunchEndChapter() { }
+    public void DropLootMask() { }
+    public void DropLootMontre() { }
 }
 
 public interface IMainVariableSource
 {
 	void OnBordingSelectPhotoFace(int index);
+    void LaunchGameSpotGerman();
+    void LaunchCardGame();
+    void LaunchEndChapter();
+    void DropLootMask();
+    void DropLootMontre();
 }
--- Assets/GAME/Script/DialogueSystem/MainVariableSourceMng.cs
+++ Assets/GAME/Script/DialogueSystem/MainVariableSourceMng.cs
@@ -7,48 +7,69 @@ using UnityEngine;
 
 public class MainVariableSourceMng : VariableSourceManager, IMainVariableSource
 {
-	public MainVariableSource source = new MainVariableSource();
+    [SerializeField] OnBoardingMain onBoardingMain;
+    public MainVariableSource source = new MainVariableSource();
 
-	#region Values function
-	public override FieldInfo[] GetSourceFieldInfo()
-	{
-		return source.GetFieldInfos();
-	}
+    #region Values function
+    public override FieldInfo[] GetSourceFieldInfo()
+    {
+        return source.GetFieldInfos();
+    }
 
-	public override object GetValue(string name)
-	{
-		return source.GetValue(name);
-	}
+    public override object GetValue(string name)
+    {
+        return source.GetValue(name);
+    }
 
-	public override object GetValue(object obj)
-	{
-		return source.GetValue(obj);
-	}
+    public override object GetValue(object obj)
+    {
+        return source.GetValue(obj);
+    }
 
-	public override void SetValue(VariableObject variable)
-	{
-		source.SetValue(variable.name, variable.valueStr);
-	}
+    public override void SetValue(VariableObject variable)
+    {
+        source.SetValue(variable.name, variable.valueStr);
+    }
 
-	public override void SetValue(string variableName, object value)
-	{
-		source.SetValue(variableName, value);
-	}
+    public override void PlayMethod(MethodObject methodObj)
+    {
+        Type type = this.GetType();
+        MethodInfo method = type.GetMethod((string)methodObj.name);
+        //has parameter
+        if (methodObj.hasParameter)
+            method.Invoke(this, new[] { methodObj.parameter.value });
+        else
+            method.Invoke(this, null);
+    }
+    #endregion
 
-	public override void PlayMethod(MethodObject methodObj)
-	{
-		Type type = this.GetType();
-		MethodInfo method = type.GetMethod((string)methodObj.name);
-		//has parameter
-		if (methodObj.hasParameter)
-			method.Invoke(this, new[] { methodObj.parameter.value });
-		else
-			method.Invoke(this, null);
-	}
-	#endregion
+    public void OnBordingSelectPhotoFace(int index)
+    {
+        onBoardingMain.afficherPhoto(index);
+    }
 
-	public void OnBordingSelectPhotoFace(int index)
-	{
-		FindObjectOfType<OnBoardingMain>().afficherPhoto(index);
-	}
+    public void LaunchGameSpotGerman()
+    {
+
+    }
+
+    public void LaunchCardGame()
+    {
+
+    }
+
+    public void LaunchEndChapter()
+    {
+
+    }
+
+    public void DropLootMask()
+    {
+
+    }
+
+    public void DropLootMontre()
+    {
+
+    }
 }
