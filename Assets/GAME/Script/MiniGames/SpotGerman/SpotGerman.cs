aeb8400c0c558f614f0a51e34fcc9d7e41ae3810
sjiaWang <s.wang@wewanttoknow.com>
2020-06-14 17:06:19 +0200

    import new graphic style

---
+++ Assets/GAME/Script/MiniGames/SpotGerman/SpotGerman.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpotGerman : MiniGameBase
+{
+	public void Show()
+	{
+		gameObject.SetActive(true);
+	}
+
+	internal void ShootTarget()
+	{
+		Debug.Log("ahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh");
+		GameComplete();
+
+		gameObject.SetActive(false);
+	}
+}
---
+++ Assets/GAME/Script/MiniGames/SpotGerman/SpotGermanSpot.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpotGermanSpot : MonoBehaviour
+{
+	[SerializeField] SpotGerman gameMain;
+	SpotGermanTarget targetSpoted;
+	private void Update()
+	{
+		var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		transform.position = Vector2.Lerp(transform.position, mousePosition, 30 * Time.deltaTime);
+
+		if (Input.GetMouseButtonDown(0))
+			if (targetSpoted != null)
+				gameMain.ShootTarget();
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.GetComponent<SpotGermanTarget>() != null)
+			targetSpoted = collision.GetComponent<SpotGermanTarget>();
+	}
+
+	private void OnTriggerExit2D(Collider2D collision)
+	{
+		if (collision.GetComponent<SpotGermanTarget>() != null)
+			targetSpoted = null;
+	}
+}
